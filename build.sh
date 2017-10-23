40ddeff342d7d59add1892b5ab4ade6153cdbb35
barisozcelik <ozcelik.baris@metu.edu.tr>
2017-10-23 22:32:29 +0300

    sad

--- build.sh
+++ build.sh
@@ -2,50 +2,40 @@
 
 # Setting up build env
 sudo yum update -y
-sudo yum install -y git cmake gcc-c++ gcc python36-devel chrpath
+sudo yum install -y git cmake gcc-c++ gcc python-devel chrpath
 mkdir -p lambda-package/cv2 build/numpy
 
 # Build numpy
-python36 -m pip install --install-option="--prefix=$PWD/build/numpy" numpy
+pip install --install-option="--prefix=$PWD/build/numpy" numpy
 cp -rf build/numpy/lib64/python2.7/site-packages/numpy lambda-package
 
 # Build OpenCV 3.2
 (
 	NUMPY=$PWD/lambda-package/numpy/core/include
 	cd build
-	git clone https://github.com/opencv/opencv.git
-	cd opencv 
-	git checkout 3.3.0 
-	cd ..
-	git clone https://github.com/opencv/opencv_contrib.git
-	cd opencv_contrib
-	git checkout 3.3.0
-	cd ..
+	git clone https://github.com/Itseez/opencv.git
 	cd opencv
-	mkdir build
-	cd build
+	git checkout 3.2.0
 	cmake										\
-		 -D CMAKE_BUILD_TYPE=RELEASE				\
-		 -D WITH_TBB=ON							\
-		 -D WITH_IPP=ON							\
-		 -D WITH_V4L=ON							\
-		 -D ENABLE_AVX=ON						\
-		 -D ENABLE_SSSE3=ON						\
-		 -D ENABLE_SSE41=ON						\
-		 -D ENABLE_SSE42=ON						\
-		 -D ENABLE_POPCNT=ON						\
-		 -D ENABLE_FAST_MATH=ON					\
-		 -D BUILD_EXAMPLES=OFF					\
-		 -D BUILD_TESTS=OFF						\
-		 -D BUILD_PERF_TESTS=OFF				\
-		 -D PYTHON3_NUMPY_INCLUDE_DIRS="$NUMPY"	\
-		 ..
-	make -j 'cat /proc/cpuinfo | grep MHz | wc -l'
+		-D CMAKE_BUILD_TYPE=RELEASE				\
+		-D WITH_TBB=ON							\
+		-D WITH_IPP=ON							\
+		-D WITH_V4L=ON							\
+		-D ENABLE_AVX=ON						\
+		-D ENABLE_SSSE3=ON						\
+		-D ENABLE_SSE41=ON						\
+		-D ENABLE_SSE42=ON						\
+		-D ENABLE_POPCNT=ON						\
+		-D ENABLE_FAST_MATH=ON					\
+		-D BUILD_EXAMPLES=OFF					\
+		-D BUILD_TESTS=OFF						\
+		-D BUILD_PERF_TESTS=OFF					\
+		-D PYTHON2_NUMPY_INCLUDE_DIRS="$NUMPY"	\
+		.
+	make -j`cat /proc/cpuinfo | grep MHz | wc -l`
 )
-
-cd /tmp
-cp build/opencv/build/lib/cv2.so lambda-package/cv2/__init__.so
-cp -L build/opencv/build//lib/*.so.3.3 lambda-package/cv2
+cp build/opencv/lib/cv2.so lambda-package/cv2/__init__.so
+cp -L build/opencv/lib/*.so.3.2 lambda-package/cv2
 strip --strip-all lambda-package/cv2/*
 chrpath -r '$ORIGIN' lambda-package/cv2/__init__.so
 touch lambda-package/cv2/__init__.py
--- run.sh
+++
@@ -1,8 +0,0 @@
-yum update -y
-yum install -y git cmake gcc-c++ gcc python36-devel chrpath
-cd /tmp
-wget https://github.com/barisozcelik/aws-lambda-python-opencv/archive/master.zip
-unzip master.zip
-chmod 777 aws-lambda-python-opencv-master
-cd aws-lambda-python-opencv-master
-su -c './build.sh' ec2-user
